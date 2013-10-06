21213c57b3f5dd335f02b1c693209851372f4b80
Martin Sandström <martin@marteinn.se>
2013-10-06 23:35:42 +0200

    Initial commit

---
+++ .bash_profile
@@ -0,0 +1,6 @@
+# includes
+[ -n "$PS1" ] && source ~/.paths
+[ -n "$PS1" ] && source ~/.exports
+[ -n "$PS1" ] && source ~/.aliases
+
+# source /usr/local/bin/virtualenvwrapper.sh
---
+++ script/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# general
+ln -s ~/dotfiles/.paths ~/.paths
+ln -s ~/dotfiles/.aliases ~/.aliases
+ln -s ~/dotfiles/.exports ~/.exports
+ln -s ~/dotfiles/.functions ~/.functions
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+
+ln -s ~/dotfiles/bin ~/bin
+
+# screen
+ln -s ~/dotfiles/.screenrc ~/.screenrc
+
+# git
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/.gitignore ~/.gitignore
